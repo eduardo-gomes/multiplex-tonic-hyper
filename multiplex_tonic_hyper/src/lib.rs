412e97d88963e2c5a4c0590c8c5ccaf414d48bb8
Eduardo M Gomes <me@emg.dev>
2022-09-08 14:47:32 +0000

    Implemented Multiplexer::call and some adapters.
    Created a Future enum to hold inner futures.
    Created Body enum to hold inner bodies.
    Implemented required methods for both.
    Added pin_project as dependencies, used to pin enum variants.

--- multiplex_tonic_hyper/src/lib.rs
+++ multiplex_tonic_hyper/src/lib.rs
@@ -1,9 +1,7 @@
-use std::{
-	future::{ready, Ready},
-	task::Poll,
-};
+use std::{future::Future, task::Poll};
 
-use hyper::{Body, Request};
+use hyper::{body::HttpBody, Body, Request, Response};
+use pin_project::pin_project;
 use tower::Service;
 
 /// Service that routes to a gRPC service and other service
@@ -24,19 +22,20 @@ where
 		Multiplexer { grpc, web }
 	}
 }
-
+type BoxedError = Box<dyn std::error::Error + Send + Sync + 'static>;
 impl<Grpc, Web> Service<Request<Body>> for Multiplexer<Grpc, Web>
 where
-	Grpc: Service<Request<Body>>,
-	Web: Service<Request<Body>>,
+	//Each type is a Service<> with its own Body type
+	Grpc: Service<Request<Body>, Response = Response<Body>>,
+	Web: Service<Request<Body>, Response = Response<Body>>,
 	//Inner errors can be converted to our error type
-	Grpc::Error: Into<Box<dyn std::error::Error + Send + Sync + 'static>>,
-	Web::Error: Into<Box<dyn std::error::Error + Send + Sync + 'static>>,
+	Grpc::Error: Into<BoxedError>,
+	Web::Error: Into<BoxedError>,
 {
-	type Response = ();
+	type Response = Response<EncapsulatedBody<Body, Body>>;
 	///Generic error that can be moved between threads
-	type Error = Box<dyn std::error::Error + Send + Sync + 'static>;
-	type Future = Ready<Result<Self::Response, Self::Error>>;
+	type Error = BoxedError;
+	type Future = EncapsulatedFuture<Grpc::Future, Web::Future>;
 
 	///Call inner services poll_ready, and propagate errors.
 	/// Only is ready if both are ready.
@@ -53,8 +52,106 @@ where
 		}
 	}
 
-	fn call(&mut self, _req: Request<Body>) -> Self::Future {
-		ready(Ok(()))
+	fn call(&mut self, req: Request<Body>) -> Self::Future {
+		let is_grpc = req
+			.headers()
+			.get("content-type")
+			.map(|x| x.as_bytes().starts_with(b"application/grpc"))
+			.unwrap_or_default();
+		if is_grpc {
+			EncapsulatedFuture::Grpc(self.grpc.call(req))
+		} else {
+			EncapsulatedFuture::Web(self.web.call(req))
+		}
+	}
+}
+
+/// Type to encapsulate both inner services Futures
+///
+///Because [poll(cx)][Future::poll] uses a pinned mutable reference,
+/// this enum needs to project the pin.
+/// That way is possible to call poll on the inner future
+#[pin_project(project = EncapsulatedProj)]
+pub enum EncapsulatedFuture<GrpcFuture, WebFuture> {
+	Grpc(#[pin] GrpcFuture),
+	Web(#[pin] WebFuture),
+}
+/// This implementation should map the response and the error from the inner futures
+///
+/// The response has its body mapped to another enum, the enum should implement `HttpBody`
+///
+impl<GrpcFuture, WebFuture, GrpcResponseBody, WebResponseBody, GrpcError, WebError> Future
+	for EncapsulatedFuture<GrpcFuture, WebFuture>
+where
+	GrpcFuture: Future<Output = Result<Response<GrpcResponseBody>, GrpcError>>,
+	WebFuture: Future<Output = Result<Response<WebResponseBody>, WebError>>,
+	GrpcError: Into<BoxedError>,
+	WebError: Into<BoxedError>,
+{
+	/// We should output `Result<Response<impl HttpBody>, Multiplexer::Error>`
+	type Output = Result<Response<EncapsulatedBody<GrpcResponseBody, WebResponseBody>>, BoxedError>;
+
+	fn poll(self: std::pin::Pin<&mut Self>, cx: &mut std::task::Context<'_>) -> Poll<Self::Output> {
+		match self.project() {
+			EncapsulatedProj::Grpc(future) => future
+				.poll(cx)
+				.map_ok(|v| v.map(|val| EncapsulatedBody::Grpc(val)))
+				.map_err(|e| e.into()),
+			EncapsulatedProj::Web(future) => future
+				.poll(cx)
+				.map_ok(|v| v.map(|val| EncapsulatedBody::Web(val)))
+				.map_err(|e| e.into()),
+		}
+	}
+}
+
+/// Type to encapsulate both inner services HttpBody types
+///
+/// Because [poll_data(cx)][HttpBody::poll_data] and [poll_trailers(cx)][HttpBody::poll_trailers] uses pinned reference, this enum needs to project the pin.
+///
+/// This enum is used as the body type in [EncapsulatedFuture].
+#[pin_project(project = BodyProj)]
+pub enum EncapsulatedBody<GrpcBody, WebBody> {
+	Grpc(#[pin] GrpcBody),
+	Web(#[pin] WebBody),
+}
+impl<GrpcBody, WebBody, GrpcError, WebError> HttpBody for EncapsulatedBody<GrpcBody, WebBody>
+where
+	GrpcBody: HttpBody<Error = GrpcError>,
+	WebBody: HttpBody<Error = WebError>,
+	GrpcBody::Error: Into<BoxedError>,
+	WebBody::Error: Into<BoxedError>,
+	GrpcBody::Data: Into<hyper::body::Bytes>,
+	WebBody::Data: Into<hyper::body::Bytes>,
+{
+	type Data = hyper::body::Bytes;
+
+	type Error = BoxedError;
+
+	fn poll_data(
+		self: std::pin::Pin<&mut Self>,
+		cx: &mut std::task::Context<'_>,
+	) -> Poll<Option<Result<Self::Data, Self::Error>>> {
+		match self.project() {
+			BodyProj::Grpc(body) => body
+				.poll_data(cx)
+				.map_ok(|data| data.into())
+				.map_err(|e| e.into()),
+			BodyProj::Web(body) => body
+				.poll_data(cx)
+				.map_ok(|data| data.into())
+				.map_err(|e| e.into()),
+		}
+	}
+
+	fn poll_trailers(
+		self: std::pin::Pin<&mut Self>,
+		cx: &mut std::task::Context<'_>,
+	) -> Poll<Result<Option<hyper::HeaderMap>, Self::Error>> {
+		match self.project() {
+			BodyProj::Grpc(body) => body.poll_trailers(cx).map_err(|e| e.into()),
+			BodyProj::Web(body) => body.poll_trailers(cx).map_err(|e| e.into()),
+		}
 	}
 }
 
@@ -62,9 +159,12 @@ where
 mod tests {
 	use std::{convert::Infallible, future::ready};
 
-	use crate::Multiplexer;
-	use hyper::{service::service_fn, Body, Request, Response};
-	use tower::ServiceExt; // ServiceExt provides ready()
+	use crate::{EncapsulatedBody, Multiplexer};
+	use hyper::{
+		body::HttpBody, header::CONTENT_TYPE, service::service_fn, Body, HeaderMap, Request,
+		Response,
+	};
+	use tower::{Service, ServiceExt}; // ServiceExt provides ready()
 
 	//This test only checks if this compiles
 	#[test]
@@ -97,5 +197,97 @@ mod tests {
 		let mut multiplex = Multiplexer::new(grpc, web);
 
 		multiplex.ready().await.unwrap();
+	}
+
+	// #[ignore = "While EncapsulatedBody is not implemented"]
+	#[tokio::test]
+	async fn multiplexer_request_to_web() {
+		let generate_service = |string: &'static str| {
+			service_fn(|_req: Request<Body>| {
+				ready(Ok::<Response<Body>, Infallible>(Response::new(Body::from(
+					string.to_owned(),
+				))))
+			})
+		};
+		let grpc = generate_service("gRPC service");
+		let web = generate_service("web service");
+		let mut multiplex = Multiplexer::new(grpc, web);
+		multiplex.ready().await.unwrap();
+		{
+			//Request web
+			let request = Request::new(Body::empty());
+			let response = multiplex.call(request).await.unwrap();
+			let content = hyper::body::to_bytes(response.into_body()).await.unwrap();
+
+			assert_ne!(content.len(), 0);
+			assert_eq!(content, "web service");
+		}
+		multiplex.ready().await.unwrap();
+		{
+			//Request grpc
+			let request = Request::builder()
+				.header(CONTENT_TYPE, "application/grpc")
+				.body(Body::empty())
+				.unwrap();
+			let response = multiplex.call(request).await.unwrap();
+			let content = hyper::body::to_bytes(response.into_body()).await.unwrap();
+
+			assert_ne!(content.len(), 0);
+			assert_eq!(content, "gRPC service");
+		}
+	}
+
+	#[tokio::test]
+	async fn encapsulated_body_poll_data_grpc() {
+		let string = "body grpc";
+		let body = EncapsulatedBody::<Body, Body>::Grpc(Body::from(string));
+
+		let data = hyper::body::to_bytes(body).await.unwrap();
+		assert_eq!(data, string);
+	}
+
+	#[tokio::test]
+	async fn encapsulated_body_poll_data_web() {
+		let string = "body web";
+		let body = EncapsulatedBody::<Body, Body>::Grpc(Body::from(string));
+
+		let data = hyper::body::to_bytes(body).await.unwrap();
+		assert_eq!(data, string);
+	}
+
+	#[tokio::test]
+	async fn encapsulated_body_poll_trailers_grpc() {
+		let (mut sender, body) = Body::channel();
+		let mut header_map = HeaderMap::new();
+		header_map.insert("From", "grpc sender".parse().unwrap());
+		let header_map = header_map;
+		sender.send_trailers(header_map.clone()).await.unwrap();
+
+		let mut body = EncapsulatedBody::<Body, Body>::Grpc(body);
+
+		let headers = body
+			.trailers()
+			.await
+			.unwrap()
+			.expect("Should return trailers!");
+		assert_eq!(headers, header_map);
+	}
+
+	#[tokio::test]
+	async fn encapsulated_body_poll_trailers_web() {
+		let (mut sender, body) = Body::channel();
+		let mut header_map = HeaderMap::new();
+		header_map.insert("From", "web sender".parse().unwrap());
+		let header_map = header_map;
+		sender.send_trailers(header_map.clone()).await.unwrap();
+
+		let mut body = EncapsulatedBody::<Body, Body>::Web(body);
+
+		let headers = body
+			.trailers()
+			.await
+			.unwrap()
+			.expect("Should return trailers!");
+		assert_eq!(headers, header_map);
 	}
 }
